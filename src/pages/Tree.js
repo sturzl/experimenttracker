94433faef4b46fa39086fb70c05b395f786cf34c
Alexandria Kudlinski <akudlins@ford.com>
2022-12-01 16:50:17 -0500

    very ugly but tree is there

--- src/pages/Tree.js
+++ src/pages/Tree.js
@@ -1,3 +1,41 @@
+import TreeView from '../Component/TreeView'
+
+
+const data = {
+    "name": "Product",
+    "children": [
+      {
+        "name": "Outcome 1"
+      },
+      {
+        "name": "Outcome 2",
+        "children": [
+          {
+            "name": "Opp"
+          },
+          {
+            "name": "Opp",
+            "children": [{
+                "name" : "Solution 1",
+                "children": [{"name": "experiment"}, {"name": "experiment"}]
+            },{
+                "name" : "Solution 2",
+                "children": [{"name": "experiment"}]
+            }]
+          },
+          {
+            "name": "Opp"
+          },
+        ]
+      },
+      {
+        "name": "Outcome 3"
+      },
+    ]
+  }
+
 export default function Tree() {
-    return <h1>Tree</h1>
+    return (<><h1>Tree</h1>
+{TreeView(data)}
+</>)
 }
