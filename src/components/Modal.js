17cb3b504fa630ce157b622687b96f097e634641
Alexandria Kudlinski <akudlins@ford.com>
2022-12-02 10:18:10 -0500

    simplified modal code and removed it from the tree for now

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,27 +1,8 @@
 import React from 'react';
 
-export default function Modal(title, children, editing, setEditing, onExited, showCloseIcon, className) {
+export default function Modal(title) {
   return (<div>
     <h5>{title}</h5>
   </div>
   );
 }
-
-{/* <div>
-<div>
-  {showCloseIcon && (
-    <button
-      aria-label="close dialog"
-      onClick={() => setEditing(false)}
-      className="dialog-close"
-    >
-      X
-    </button>
-  )}
-  <h5>
-    {title}
-  </h5>
-
-  {children}
-</div>
-</div> */}
--- src/components/TreeView.js
+++ src/components/TreeView.js
@@ -1,37 +1,24 @@
 import Tree from 'react-d3-tree';
-import Modal from './Modal'
-import React, { useState } from 'react';
+import React  from 'react';
 
 import '../styles/TreeView.scss'
 
 
 function RenderCustomNode({nodeDatum, toggleNode, foreignObjectProps}) {
-  const [isOpen, setIsOpen] = useState(foreignObjectProps.showModal);
-isOpen && <Modal title="Experiment Details" editing={false} setEditing={setIsOpen} showCloseIcon={true}/>
 
 return ( <g>
   <foreignObject {...foreignObjectProps}>
   <div className='node'> 
     <h1 style={{textAlign:"center"}}>{nodeDatum.name}</h1>
-    {console.log(nodeDatum)}
     {nodeDatum.children && 
     <><h3>{nodeDatum.children.length}</h3>
     <button width="100%" onClick={toggleNode}>
     {nodeDatum.__rd3t.collapsed ? "Open" : "Close"}
       </button></>}
-      {nodeDatum.name === "experiment" && <button width="100%" onClick={foreignObjectProps.setIsOpen(true)}>Details</button>}
-        {nodeDatum.attributes &&
-              Object.keys(nodeDatum.attributes).map((labelKey, i) => (
-                <li key={`${labelKey}-${i}`}>
-                  {labelKey}: {nodeDatum.attributes[labelKey]}
-                </li>
-              ))}      
     </div>
   </foreignObject>
 </g>)
 }
-
-
 
 export default function TreeView(data) {
   const nodeSize = { x: 200, y: 200 };
