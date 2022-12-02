f44c08742c8230c99ac35629bce026a93c8a1b8f
Avery Sturzl <msturzl@ford.com>
2022-12-02 16:36:01 -0500

    add analytics

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './styles/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
+import { inject } from '@vercel/analytics';
+
+inject();
 
 const rootReference = document.getElementById('root');
 const root = createRoot(rootReference);
