4c28feb3f8b7bd3d520b4bdbeeddf94ee1fe89a4
Avery Sturzl <averysturzl@gmail.com>
2022-12-02 13:32:34 -0500

    blah

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,9 @@ export default function Header() {
     return (
         <header className='header'>
             <Link className='app-name' to={"/"}>Experiment&nbsp;IQ</Link>
-            {/* {props.activeNode && <h2 style={{ backgroundColor: "red" }}>{props.activeNode}</h2>} */}
             <nav>
                 <Link to={"/tree"}>Tree View</Link>&nbsp;&nbsp;|&nbsp;&nbsp;
+                <Link to={"/tree"}>New View</Link>&nbsp;&nbsp;|&nbsp;&nbsp;
                 <Link to={"/roadmap"}>Roadmap View</Link>&nbsp;&nbsp;|&nbsp;&nbsp;
                 <Link to={"/tree2"}>Filter Experiments</Link>
             </nav>
